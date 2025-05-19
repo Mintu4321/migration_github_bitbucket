9f8f801e23bd10808374c799b88a94d7f88b362c
Tridib Bas <89687126+Mintu4321@users.noreply.github.com>
2025-05-20 01:16:41 +0530

    Update mirrorGitHub.groovy

--- vars/mirrorGitHub.groovy
+++ vars/mirrorGitHub.groovy
@@ -1,1 +1,22 @@
+import groovy.json.JsonSlurper
+
+def call(String scriptName) {
+    withCredentials([string(credentialsId: 'bitbucket-github-json', variable: 'CRED_JSON')]) {
+        def creds = new JsonSlurper().parseText(env.CRED_JSON)
+
+        def envVars = [
+            "BITBUCKET_USERNAME=${creds.BITBUCKET_USERNAME}",
+            "PASSWORD=${creds.BITBUCKET_APP_PASSWORD}",
+            "BITBUCKET_WORKSPACE=${creds.BITBUCKET_WORKSPACE}",
+            "GITHUB_USER=${creds.GITHUB_USER}"
+        ]
+
+        echo "📢 Executing Python script: ${scriptName}"
+        echo "🔐 Using user: ${creds.BITBUCKET_USERNAME} and GitHub: ${creds.GITHUB_USER}"
+
+        withEnv(envVars) {
+            sh "python3 ${scriptName}"
+        }
+    }
+}
 
